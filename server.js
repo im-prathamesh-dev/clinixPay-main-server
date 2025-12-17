7f78787ff4b4619a7faa21eb83e961de06ca3511
im-prathamesh-dev <prathamesh0755@gmail.com>
2025-12-17 14:16:51 +0530

    feat: add CORS support and update authentication routes

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connection");
 const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 /* Middleware */
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
